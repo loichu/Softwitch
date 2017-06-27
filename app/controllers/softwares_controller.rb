727313f2ba7fae7564062e436dcd16c09e5dc6ac
Loic Humbert <loic.humbert@epfl.ch>
2017-06-27 10:13:32 +0200

    Icons working but not backend

--- app/controllers/softwares_controller.rb
+++ app/controllers/softwares_controller.rb
@@ -3,7 +3,7 @@ class SoftwaresController < ApplicationController
 
   def index
     @softwares = Software.all
-    CheckAllCompatibility(@softwares) #HANDLE THIS SHIT
+    CheckAllCompatibility(@softwares)
   end
 
   def show
